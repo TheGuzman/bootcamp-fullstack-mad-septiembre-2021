8a485ce93e4949dce7a39913a7a13afd0f9458b5
Diego Guzmán Medrano <guzmanmedrano.diego@gmail.com>
2021-10-17 23:13:57 +0200

    Second function to store values in second array added. however not functioning

--- students/diego-guzman/js-practice/js-todo-list-app/app.js
+++ students/diego-guzman/js-practice/js-todo-list-app/app.js
@@ -1,12 +1,27 @@
 const todoContainer = document.querySelector('.todo__container__list');
 const doneContainer = document.querySelector('.done__container__list');
-const arrayTasks = [];
+const arrayAllTasks = [];
+const arrayNewTasks = [];
 
 // todoContainer.appendChild(arrayTasks[2][1]);
 // if(sessionStorage.getItem('tasks') !== null){
 //     const storedTasks = JSON.parse(sessionStorage.getItem('tasks'));
 //     updateDOMStats(storedTasks);
 // }
+if (localStorage.getItem('newtasks') !== null) {
+    const storedTasks = JSON.parse(localStorage.getItem('newtasks'));
+    arrayAllTasks.push([arrayNewTasks]);
+    localStorage.setItem('tasks', JSON.stringify(arrayAllTasks));
+    checktasks(storedTasks);
+}
+function checktasks(arr) {
+    arr.forEach(v=>{
+        const prevTask = document.createElement('li');
+        prevTask.textContent = localStorage.getItem('newtasks', JSON.stringify(arrayAllTasks[v]));
+        todoContainer.appendChild(prevTask);
+    })
+}
+
 
 document.querySelector('.form__container').addEventListener('submit', e => {
     e.preventDefault();
@@ -21,9 +36,41 @@ document.querySelector('.form__container').addEventListener('submit', e => {
     newTask.textContent = inputTaskName.value + ':' + ' ' + inputTaskDescription.value;
     todoContainer.appendChild(newTask);
     newTask.insertAdjacentElement('beforeend', taskDoneChecker);
-    arrayTasks.push([inputTaskName.value, inputTaskDescription.value, false]);
-    localStorage.setItem('tasks',JSON.stringify(arrayTasks));
+    arrayNewTasks.push([inputTaskName.value, inputTaskDescription.value, false]);
+    localStorage.setItem('newtasks', JSON.stringify(arrayNewTasks));
     formElement.task.value = '';
     formElement.description.value = '';
-});
+})
 
+
+
+
+
+
+
+// function checktasks(arr){
+//     for (let i =0; i<arr.length; i++){
+//         for(let j=0; j<arr[i].length; j++){
+//         }
+//     }
+//     }
+
+
+
+// function addEntry() {
+//     // Parse any JSON previously stored in allEntries
+//     const existingTasks = JSON.parse(localStorage.getItem("tasks"));
+//     if (existingTasks == null) arrayTasks = [];
+//     const newTask = document.createElement('li'); //Added list
+//     newTask.textContent = inputTaskName.value + ':' + ' ' + inputTaskDescription.value;
+//     todoContainer.appendChild(newTask);
+//     newTask.insertAdjacentElement('beforeend', taskDoneChecker);
+//     arrayTasks.push([inputTaskName.value, inputTaskDescription.value, false]);
+//     localStorage.setItem('tasks', JSON.stringify(arrayTasks));
+//     formElement.task.value = '';
+//     formElement.description.value = ''
+//     localStorage.setItem("newTask", JSON.stringify(newTask));
+//     // Save allEntries back to local storage
+//     existingTasks.push(newTask);
+//     localStorage.setItem("tasks", JSON.stringify(existingTasks));
+// };
